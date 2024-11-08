d1a4982eac54357368fd1e8a7ff2fad6abcc31ee
jtorsuarz <j.torsuarz@gmail.com>
2024-11-08 18:21:04 +0100

    initial structure

---
+++ lib/features/routines/presentation/bloc/routine_bloc.dart
@@ -0,0 +1,13 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'routine_event.dart';
+part 'routine_state.dart';
+
+class RoutineBloc extends Bloc<RoutineEvent, RoutineState> {
+  RoutineBloc() : super(RoutineInitial()) {
+    on<RoutineEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
---
+++ lib/features/routines/presentation/bloc/routine_state.dart
@@ -0,0 +1,6 @@
+part of 'routine_bloc.dart';
+
+@immutable
+sealed class RoutineState {}
+
+final class RoutineInitial extends RoutineState {}
