4eefce1de4601f315a98438491917bc7132e6fd3
jtorsuarz <j.torsuarz@gmail.com>
2025-01-26 18:04:40 +0100

    disbled mod debug

--- lib/core/constants/app_dotenv.dart
+++ lib/core/constants/app_dotenv.dart
@@ -1,8 +1,8 @@
 class AppDotEnv {
-  static bool activeAppLogger = true;
+  static bool activeAppLogger = false;
   static bool isDarkMode = false;
-  static const isDebugModeApp = true;
-  static const isDebugModeWorkManager = true;
+  static const isDebugModeApp = false;
+  static const isDebugModeWorkManager = false;
   static const String appVersion = '1.0.0';
   static int selectedColorInitial = 2;
   static String notificationUniqueChannelId = 'pasti_track_channel';
